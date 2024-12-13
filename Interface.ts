9c42ae42eb5b30a8624badbb8e8fcb87c645844d
bjl0615 <bjl0615@gmail.com>
2024-12-13 13:52:02 +0900

    Type Script Interface

---
+++ Interface.ts
@@ -0,0 +1,28 @@
+// 인터페이스(Interface)
+
+// 선택적 속서 - ?
+// 읽기전용 속성 - readonly
+
+interface User {
+  name : string , 
+  readonly age : number ,
+  isValid? : boolean
+}
+
+const heropy : User = {
+  name : 'Heropy' ,
+  age : 85 ,
+  isValid : true
+} 
+
+heropy.isValid = false;
+heropy.age = 22
+
+const neo : User = {
+  name : 'Neo' ,
+  age : 102
+}
+ 
+// 함수 타입 - 호출 시그니처(Call Signature)
+// 인덱스 가능 타입 - 인덱스 시그니처(Index Signature)
+// 확장(상속)
