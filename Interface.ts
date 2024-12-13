7c184766d5bf13935a99f478f5af1b4a6920a14b
bjl0615 <bjl0615@gmail.com>
2024-12-13 14:01:03 +0900

    Call Signature Interface

--- Interface.ts
+++ Interface.ts
@@ -3,26 +3,46 @@
 // 선택적 속서 - ?
 // 읽기전용 속성 - readonly
 
+// interface User {
+//   name : string , 
+//   readonly age : number ,
+//   isValid? : boolean
+// }
+// 
+// const heropy : User = {
+//   name : 'Heropy' ,
+//   age : 85 ,
+//   isValid : true
+// } 
+// 
+// heropy.isValid = false;
+// heropy.age = 22
+// 
+// const neo : User = {
+//   name : 'Neo' ,
+//   age : 102
+// }
+ 
+// 함수 타입 - 호출 시그니처(Call Signature)
+
+interface GetName {
+  (message : string) : string
+}
 interface User {
   name : string , 
   readonly age : number ,
-  isValid? : boolean
+  getName : GetName
 }
 
 const heropy : User = {
   name : 'Heropy' ,
   age : 85 ,
-  isValid : true
-} 
-
-heropy.isValid = false;
-heropy.age = 22
-
-const neo : User = {
-  name : 'Neo' ,
-  age : 102
+  getName(message : string) {
+    console.log(message)
+    return this.name
+  }
 }
- 
-// 함수 타입 - 호출 시그니처(Call Signature)
+heropy.getName('Hello~')
+
 // 인덱스 가능 타입 - 인덱스 시그니처(Index Signature)
 // 확장(상속)
